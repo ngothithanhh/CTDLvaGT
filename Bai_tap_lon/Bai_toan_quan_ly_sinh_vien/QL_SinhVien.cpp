3acdf132ff231a697ec55a21490ea7fb90a532db
Thanh cuti <ngothanhthsctanhung@gmail.com>
2024-10-21 12:36:09 +0700

    update

--- Bai_tap_lon/Bai_toan_quan_ly_sinh_vien/QL_SinhVien.cpp
+++ Bai_tap_lon/Bai_toan_quan_ly_sinh_vien/QL_SinhVien.cpp
@@ -1,6 +1,6 @@
 //Quan li sinh vien bang vector
 
-#include<bits/stdc++.h>
+#include<iostream>
 #include "vector.cpp"
 
 using namespace std;
@@ -44,6 +44,10 @@ class SinhVien{
             return gioiTinh;
         }
 
+        int getMaSV(){
+            return maSV;
+        }
+
         int getTuoi(){
             return tuoi;
         }
@@ -56,16 +60,108 @@ class SinhVien{
             return lop;
         }
 
+        void suaThongTin(){
+            cin >> *this;
+        }
+
 };
 
 
 class DS_SinhVien{
     private:
-        vector<SinhVien> dsVector;
+        Vector<SinhVien> ds;
 
     public:
 
-        void add
+        void themSV(){
+            char tieptuc = 'c';
+            while (tieptuc){
+                SinhVien sv;
+                cin >> sv;
+                ds.push_back(sv);
+                cout << "Co nhap nua khong? (c/k): "; cin >> tieptuc;
+            }
+        }
 
+        void xoaSV(int& maSV){
+            int check = 0;
+            for(int i = 0; i < ds.size(); i++){
+                if(ds[i].getMaSV() == maSV){
+                    check++;
+                    ds.erase(i);
+                    break;
+                }
+            }
+
+            if(check == 0){
+                cout << "Khong tim thay sinh vien co ma: " << maSV << "\n";
+            }
+            else{
+                cout << "Da xoa\n";
+            }
+        }
+
+        void suaThongTinSV(int& maSV){
+            for(int i = 0; i < ds.size(); i++){
+                if(ds[i].getMaSV() == maSV){
+                    cout << ds[i] << "\n";
+                    cout << "Nhap thong tin moi cho sinh vien: ";
+                    ds[i].suaThongTin();
+                }
+            }
+        }
+
+        void sapXepSV(){
+            for(int i = 0; i < ds.size() - 1; i++){
+                for(int j = i + 1; j < ds.size(); j++){
+                    if(ds[i].getTen() > ds[j].getTen()){
+                        swap(ds[i], ds[j]);
+                    }
+                }
+            }
+
+            cout << "Da sap xep\n";
+        }
+
+        void hienThiDSSV(){
+            cout << "Danh sach sinh vien co trong vector:\n";
+            for(int i = 0; i < ds.size(); i++){
+                cout << ds[i] << "\n";
+            }
+        }
+
+        void timKiemSV(string& ten){
+            int check = 0;
+            for(int i = 0; i < ds.size(); i++){
+                if(ds[i].getTen() == ten){
+                    check++;
+                    cout << ds[i] << "\n";
+                }
+            }
+
+            if(check){
+                cout << "Khong tim thay sinh vien co ten: " << ten << "\n";
+            }
+        }
+};
+
+class App{
+    private:
+        DS_SinhVien qlSV;
+
+    public:
+        void menu(){
+            int choice;
+            cout << "\n===== Menu Quan Ly Sinh Vien =====\n";
+            cout << "1. Nhap danh sach sinh vien\n";
+            cout << "2. Hien thi danh sach sinh vien\n";
+            cout << "3. Xoa sinh vien\n";
+            cout << "4. Sua thong tin sinh vien\n";
+            cout << "5. Sap xep sinh vien theo ho ten\n";
+            cout << "6. Tim kiem sinh vien theo ho ten\n";
+            cout << "0. Thoat\n";
+            cout << "Nhap lua chon: ";
+            cin >> choice;
+        }
 
 };
